e5335e9cca88b08408d0ad6764b112b2c156d657
akutuzov <akutuzov72@gmail.com>
2016-12-10 16:24:29 +0100

    Visualizations.

--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 import os
-import seaborn
 import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def distance_graph(root, query, distances):
@@ -8,8 +9,8 @@ def distance_graph(root, query, distances):
     if not plotFile in os.listdir(root + 'data/distance_graphs/'):
         G = nx.Graph()
         for x in distances: 
-            if not x[1] == 1:
-                G.add_edge(x[0][0], x[0][1], weight=x[1])
+            if not x[1] == 1 and not 'all' in x[0]:
+                G.add_edge(x[0][0], x[0][1], weight=1-x[1])
         labels = nx.get_edge_attributes(G,'weight')
         edges, weights = zip(*labels.items())
         pos = nx.spring_layout(G)
@@ -18,8 +19,9 @@ def distance_graph(root, query, distances):
         nx.draw(G,pos, with_labels=True, node_size=2300, font_size=10, font_color='#1a2e1a', node_color='#84e786', \
                 alpha=0.8, node_shape="o", edgelist=edges, edge_color=weights, width=8.0, edge_cmap=plt.cm.Blues_r)
         nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+        nx.write_graphml(G, 'data/distance_graphs/'+query+'.graphml')
         plt.savefig(root + 'data/distance_graphs/' + query + '.png', dpi=150, bbox_inches='tight')
         plt.clf()
-    image_path = '/data/distance_graphs/' + plotFile
+    image_path = 'data/distance_graphs/' + plotFile
 
     return image_path
